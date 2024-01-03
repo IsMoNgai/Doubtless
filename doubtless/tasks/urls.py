99add1928e34baff85163110361164bed8e59f9d
IsMoNgai <mongaiwaiyin@gmail.com>
2024-01-03 00:09:23 -0800

    finished todo List and setup all database

--- doubtless/tasks/urls.py
+++ doubtless/tasks/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('remaining', views.remaining, name='remaining'),
-    path('delete_task', views.delete_task, name='delete_task'),
-    path('task_detail', views.task_detail, name='task_detail'),
+    path('delete_task/<str:task_id>', views.delete_task, name='delete_task'),
+    path('remove_task/<str:task_id>', views.remove_task, name='remove_task'),
+    path('task_detail/<str:task_id>', views.task_detail, name='task_detail'),
+    path('toggle_complete/<str:task_id>', views.toggle_complete, name='toggle_complete'),
     path('completed', views.completed, name='completed'),
     path('add_task', views.add_task, name='add_task'),
 ]
--- doubtless/tasks/views.py
+++ doubtless/tasks/views.py
@@ -1,24 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
 def home(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     return render(request, 'index.html', {
         'tasks' : tasks,
     })
 
 def remaining(request):
-    return render(request, 'remaining.html')
+    remaining_tasks = Task.objects.filter(completed = False)
+    return render(request, 'remaining.html', {
+        'remaining_tasks' : remaining_tasks,
+    })
 
 def completed(request):
-    return render(request, 'completed.html')
+    completed_tasks = Task.objects.filter(completed = True)
+    return render(request, 'completed.html', {
+        'completed_tasks' : completed_tasks,
+    })
 
 def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        due_time = request.POST.get('due_time')
+        completed = False
+
+        if title != "" and due_date != "" and due_time != "":
+            task = Task(
+                title = title,
+                description = description,
+                due_date = due_date,
+                due_time = due_time,
+                completed = completed
+            )
+            task.save()
+            return redirect('home')
+    else:
+        return render(request, 'add_task.html')
+
     return render(request, 'add_task.html')
 
-def delete_task(request):
-    return render(request, 'delete.html')
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    return render(request, 'delete.html', {
+        "task":task,
+    })
 
-def task_detail(request):
-    return render(request, 'task_detail.html')
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+    return render(request, 'task_detail.html', {
+        "task":task,
+    })
+
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.completed = not task.completed
+        task.save()
+        # Redirect to the previous view
+        previous_url = request.META.get('HTTP_REFERER')
+        if previous_url:
+            return redirect(previous_url)
+        else:
+            # Fallback to a default URL if the referrer is not available
+            return redirect('home')
+
+def remove_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.delete()
+        return redirect('home')
+
